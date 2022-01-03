cb8e8481156effaf48e94e519c3868d8b13c3213
Xuanji-Li <lixuanji2019@gmail.com>
2022-01-04 02:26:02 +1030

    123

---
+++ 5/main.js
@@ -0,0 +1,38 @@
+let btn=document.querySelector(".btn");
+
+
+
+let result;
+ let allNumbers= document.querySelector(".allNumbers").value;
+ let allNumber= allNumbers.split(/\s+/);
+function add(allNumber ){
+    if(allNumber.length==1)
+    {
+        result +=allNumber[0];
+    }
+    result= add(allNumber)
+    // let allNumbers= document.querySelector(".allNumbers").value;
+    // let allNumber= allNumbers.split(/\s+/);
+    // // let allIntegers= ParseInt(allNumber);
+    
+    
+    // let result=0;
+    //     for(let i=0; i<allNumber.length; i++){
+            
+    //         result+=(allNumber[i]-0);
+    //     }
+}
+function add(number)
+{
+    
+    if(number==1){
+    return 1;
+    }
+
+    if(number>1){ 
+    return add(number-1)+number;
+    }
+}
+
+let result =add(3);
+console.log(result);
